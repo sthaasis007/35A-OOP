9909dfddad1fed294e570e3f68eb21178f36d23e
sthaasis007 <aashishsresth123@gmail.com>
2024-04-11 10:16:51 +0545

    second task

--- Hello.java
+++ Hello.java
@@ -2,6 +2,15 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is my first program");
+        System.out.println("I am on module ST4003CEM");
     }
     
 }
+// 2. Extend/Modify the above program to print two additional lines:
+
+// This is my first program
+
+// I am on module ST4003CEM
+
+// System.out.print (not println) and produces the same output.
