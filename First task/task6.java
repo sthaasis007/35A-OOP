62452ab7cfd00cee552306541f262866ef244680
sthaasis007 <aashishsresth123@gmail.com>
2024-04-11 20:36:17 +0545

    made a folder

---
+++ First task/task6.java
@@ -0,0 +1,23 @@
+// 6. write a program that prints the following output:
+
+// A well-formed Java program has a main method with { and } braces.
+
+
+
+// A System.out.println statement has ( and ) and usually a String that starts and ends with a " character.
+
+// (But we type \" instead!)
+
+// What is the difference between
+
+// a ' and a "?  Or between a " and a \"?
+
+
+
+// One is what we see when we're typing our program. The other is what appears on the "console."
+public class task6 {
+    public static void main(String[] args) {
+        System.out.println("A well-formed java program has a main method with { and } braces.");
+    }
+    
+}
