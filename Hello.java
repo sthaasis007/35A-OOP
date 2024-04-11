0d584964d9f2ad68a0b07cd5f174b5541cc34b7a
sthaasis007 <aashishsresth123@gmail.com>
2024-04-11 10:14:02 +0545

    tsk 1

---
+++ Hello.java
@@ -0,0 +1,7 @@
+// question no 1. Write a program that prints Hello World! to the console. Save the program as Hello.java.
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
