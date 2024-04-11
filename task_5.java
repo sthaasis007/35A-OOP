64ab3db908170e62da301e5d3e44a8476bd412c5
sthaasis007 <aashishsresth123@gmail.com>
2024-04-11 10:51:20 +0545

    task 5

---
+++ task_5.java
@@ -0,0 +1,17 @@
+// 5. WAP prints the following pattern:
+
+// *********
+
+// *********
+
+// *********
+
+// *********
+public class task_5 {
+    public static void main(String[] args) {
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+    }
+}
