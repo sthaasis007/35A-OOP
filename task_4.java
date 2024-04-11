4e2dca1cf5b546d8fa5dfe43d3d7d05260a3ca88
sthaasis007 <aashishsresth123@gmail.com>
2024-04-11 10:39:49 +0545

    task 4

---
+++ task_4.java
@@ -0,0 +1,18 @@
+// 4. Write a Program that prints the following pattern:
+
+// *
+
+// **
+
+// ***
+
+// ****
+public class task_4 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+    
+}
