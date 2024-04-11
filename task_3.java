b0e8a6b91d8c29e27e99a3263c2583534c7970f3
sthaasis007 <aashishsresth123@gmail.com>
2024-04-11 10:27:48 +0545

    task no.3

---
+++ task_3.java
@@ -0,0 +1,27 @@
+// Write a program that prints the following output:
+
+// A "quoted" String is
+
+// 'much' better if you learn
+
+// the rules of "escape sequences."
+
+// Also, "" represents an empty
+
+// String. Don't forget: use \"
+
+// instead of " !
+
+// '' is not the same as "
+public class task_3 {
+    public static void main(String[] args) {
+        System.out.println("A \"quoted\" string is");
+        System.out.println("\'much\' better if you learn");
+        System.out.println("the rules of \"escape sequences.\"");
+        System.out.println("Also, \"\" represents an empty");
+        System.out.println("String. Don't forget: use \\");
+        System.out.println("instead of \" !");
+        System.out.println("\"' is not the same as \"");
+    }
+    
+}
